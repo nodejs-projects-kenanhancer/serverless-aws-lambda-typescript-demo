e59a6a0b9b0c1d03a1de6a08ae9f276de09638dd
kenan hancer <kenanhancer@kenans-mac-pro.home>
2021-08-16 14:36:16 +0100

    reading environment variables is updated.

--- src/domain/config/index.ts
+++ src/domain/config/index.ts
@@ -1,8 +1,16 @@
 import dotenv from 'dotenv';
 import Joi, { Schema } from 'joi';
+import { resolve } from 'path';
 
 const loadConfig = (schema: Schema) => {
-    dotenv.config();
+
+    if (process.env.NODE_ENV === 'test') {
+        const envPath = resolve(__dirname, `../../.env.${process.env.NODE_ENV}`);
+
+        dotenv.config({ path: envPath });
+    } else {
+        dotenv.config();
+    }
 
     const { value: envVars, error } = schema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
@@ -16,7 +24,6 @@ const loadConfig = (schema: Schema) => {
 const envVarsSchema = Joi.object()
     .keys({
         NODE_ENV: Joi.string().required().valid('production', 'development', 'test').default('development'),
-        // PORT: Joi.number().required().default(3000),
         SERVERLESS_STAGE: Joi.string().required().valid('prod', 'dev', 'test').default('dev'),
         COGNITO_USER_POOL_ID: Joi.string().required().description('AWS Cognito User Pool ID'),
         COGNITO_CLIENT_ID: Joi.string().required().description('AWS Cognito Client ID')
@@ -27,7 +34,6 @@ const envVars = loadConfig(envVarsSchema);
 
 export default {
     env: envVars.NODE_ENV as string,
-    // port: envVars.PORT as number,
     stage: envVars.SERVERLESS_STAGE as string,
     aws: {
         cognito: {
